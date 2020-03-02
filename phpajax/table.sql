74a1196f8c6780d650b45261be913ccbc72480f5
nrcfrld <50624614+nrcfrld@users.noreply.github.com>
2020-03-02 22:14:21 +0700

    Add files via upload

---
+++ phpajax/table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `tbl_mahasiswa` (
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `nama_mahasiswa` varchar(255) NOT NULL,
+  `alamat` text NOT NULL,
+  `jenis_kelamin` varchar(255) NOT NULL,
+  `tgl_masuk` date NOT NULL,
+  `jurusan` varchar(255) NOT NULL
+)
